d573e74d0e223461c0c4cf9f52b04465f067864a
wrk1231 <wrk1231@gmail.com>
2019-05-30 23:44:00 -0400

    Career Page

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,11 +3,58 @@
 const app = getApp()
 
 Page({
+  onReady:function(){
+    this.popup = this.selectComponent("#popup");
+  },
+  showPopup(){
+    this.popup.showPopup();
+  },
+  cl(){
+    console.log("Hello World");
+  },
+  _error(){
+    console.log('canceled');
+    this.popup.hidePopup();
+  },
+  _success(){
+    console.log('confirmed');
+    this.popup.hidePopup();
+  },
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    jobData: [
+      { "position": "01", "text": "text1"},
+      { "position": "02", "text": "text2"},
+      { "position": "03", "text": "text3"},
+      { "position": "04", "text": "text4"},
+      { "position": "05", "text": "text5"},
+    ],
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   },
   //事件处理函数
   bindViewTap: function() {
@@ -42,6 +89,10 @@ Page({
         }
       })
     }
+  
+    console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
+    console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
+    
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -50,10 +101,9 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
-})
-Page({
+  },
   clickMe() {
-    this.setData({ msg: 'Hello World' })
+    this.setData({ msg: 'H W' })
   }
 })
+
